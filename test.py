8354aee755f5623536cd9855040c41f2e3909c39
gulin.serge <gulin.serge@gmail.com>
2014-09-02 08:24:40 +0400

    styles were fixed

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from pybrain.structure import LinearLayer, SigmoidLayer
 from pybrain.structure import FeedForwardNetwork
 from pybrain.structure import FullConnection
-from pybrain.supervised.trainers import BackpropTrainer,RPropMinusTrainer
+from pybrain.supervised.trainers import BackpropTrainer, RPropMinusTrainer
 from pybrain.datasets import SupervisedDataSet
 from bot.training import days
 
@@ -11,22 +11,22 @@ n = FeedForwardNetwork()
 inLayer = LinearLayer(10)
 hiddenLayers = []
 
-for i in range(0,3):
- 	hiddenLayers.append(LinearLayer(10))
+for i in range(0, 3):
+    hiddenLayers.append(LinearLayer(10))
 
 outLayer = LinearLayer(5)
 
 n.addInputModule(inLayer)
-for layer in hiddenLayers: 
-	n.addModule(layer)
+for layer in hiddenLayers:
+    n.addModule(layer)
 n.addOutputModule(outLayer)
 
 n.addConnection(FullConnection(inLayer, hiddenLayers[0]))
 
-for i in range(1,len(hiddenLayers)):
-	n.addConnection(FullConnection(hiddenLayers[i-1], hiddenLayers[i]))
+for i in range(1, len(hiddenLayers)):
+    n.addConnection(FullConnection(hiddenLayers[i - 1], hiddenLayers[i]))
 
-n.addConnection(FullConnection(hiddenLayers[len(hiddenLayers)-1], outLayer))
+n.addConnection(FullConnection(hiddenLayers[len(hiddenLayers) - 1], outLayer))
 n.sortModules()
 
 
@@ -36,75 +36,75 @@ DS = SupervisedDataSet(10, 5)
 
 data = days('btceUSD.days.csv')
 
+
 def normalize(v, _max, _min):
-	return 2.0 / (_max - _min) * (v - _min) - 1.0
+    return 2.0 / (_max - _min) * (v - _min) - 1.0
+
 
 def denormalize(v, _max, _min):
-	return (v + 1.0) / 2.0 * (_max - _min) + _min
+    return (v + 1.0) / 2.0 * (_max - _min) + _min
+
 
 day = 0
 while day < 100:
-	window_prices = []
-	window_volumes = []
-	for i in range(0,5):
-		tick = data.next()
-		price, volume = tick[0], tick[1]
-		window_prices.append(price)
-		window_volumes.append(volume)
+    window_prices = []
+    window_volumes = []
+    for i in range(0, 5):
+        tick = data.next()
+        price, volume = tick[0], tick[1]
+        window_prices.append(price)
+        window_volumes.append(volume)
 
-	price_min = min(*window_prices)
-	price_max = max(*window_prices)
+    price_min = min(*window_prices)
+    price_max = max(*window_prices)
 
-	volume_min = min(*window_volumes)
-	volume_max = max(*window_volumes)
+    volume_min = min(*window_volumes)
+    volume_max = max(*window_volumes)
 
+    forecast = []
+    for i in range(0, 5):
+        tick = data.next()
+        price = tick[0]
+        forecast.append(price)
 
-	forecast = []
-	for i in range(0,5):
-		tick = data.next()
-		price = tick[0]
-		forecast.append(price)
+    price_max = max(price_max, *forecast)
+    price_min = min(price_min, *forecast)
 
-	price_max = max(price_max, *forecast)
-	price_min = min(price_min, *forecast)
+    for i in range(0, 5):
+        window_prices[i] = normalize(window_prices[i], price_max, price_min)
+        window_volumes[i] = normalize(window_volumes[i], volume_max, volume_min)
 
-	for i in range(0, 5):
-		window_prices[i] = normalize(window_prices[i], price_max, price_min)
-		window_volumes[i] = normalize(window_volumes[i], volume_max, volume_min)	
+    for i in range(0, 5):
+        forecast[i] = normalize(forecast[i], price_min, price_max)
 
-	for i in range(0, 5):
-		forecast[i] = normalize(forecast[i], price_min, price_max)
+    inputs = [0.0] * 10
+    inputs[0] = window_prices[0]
+    inputs[2] = window_prices[1]
+    inputs[4] = window_prices[2]
+    inputs[6] = window_prices[3]
+    inputs[8] = window_prices[4]
 
+    inputs[1] = window_volumes[0]
+    inputs[3] = window_volumes[1]
+    inputs[5] = window_volumes[2]
+    inputs[7] = window_volumes[3]
+    inputs[9] = window_volumes[4]
 
-	inputs = [0.0] * 10
-	inputs[0] = window_prices[0]
-	inputs[2] = window_prices[1]
-	inputs[4] = window_prices[2]
-	inputs[6] = window_prices[3]
-	inputs[8] = window_prices[4]
+    DS.appendLinked(inputs, forecast)
 
-	inputs[1] = window_volumes[0]
-	inputs[3] = window_volumes[1]
-	inputs[5] = window_volumes[2]
-	inputs[7] = window_volumes[3]
-	inputs[9] = window_volumes[4]
-
-	DS.appendLinked(inputs, forecast)
-
-
-	day += 1
+    day += 1
 
 
 # training
-trainer = RPropMinusTrainer(n, verbose=True, batchlearning=True,  learningrate=0.01, lrdecay=0.0, momentum=0.0, weightdecay=0.0)
+trainer = RPropMinusTrainer(n, verbose=True, batchlearning=True, learningrate=0.01, lrdecay=0.0, momentum=0.0,
+                            weightdecay=0.0)
 trainer.setData(DS)
 trainer.trainUntilConvergence(validationProportion=0.25, maxEpochs=100)
 
 # validating
 valid_data = days('btceUSD.days.csv')
-for i in range(0,105):
-	valid_data.next()
-
+for i in range(0, 105):
+    valid_data.next()
 
 window = []
 
@@ -115,18 +115,18 @@ max_volume = 0.0
 min_volume = float('inf')
 
 print "Test window: "
-for i in range(0,5):
-	tick = data.next()
-	price, volume, date = tick[0], tick[1], tick[2]
-	window.append(price)
-	window.append(volume)
-	max_price = max(max_price, price)
-	min_price = min(min_price, price)
+for i in range(0, 5):
+    tick = data.next()
+    price, volume, date = tick[0], tick[1], tick[2]
+    window.append(price)
+    window.append(volume)
+    max_price = max(max_price, price)
+    min_price = min(min_price, price)
 
-	max_volume = max(max_volume, volume)
-	min_volume = min(min_volume, volume)
+    max_volume = max(max_volume, volume)
+    min_volume = min(min_volume, volume)
 
-	print "%s | %s %s" % (date, price, volume)
+    print "%s | %s %s" % (date, price, volume)
 
 window[0] = normalize(window[0], max_price, min_price)
 window[2] = normalize(window[2], max_price, min_price)
