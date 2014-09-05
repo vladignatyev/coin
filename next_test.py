3c03b8587c2bf6871096b3cdd90cf0120a2bdd0d
gulin.serge <gulin.serge@gmail.com>
2014-09-05 20:39:55 +0400

    reformat and nexttest.predictions.csv to gitignore

--- next_test.py
+++ next_test.py
@@ -10,25 +10,26 @@ from bot.training import days
 
 
 def load_all_ticks():
-	ticks = []
-	data = days('btceUSD.days.csv')
+    ticks = []
+    data = days('btceUSD.days.csv')
 
-	try:
-		while True:
-			price, volume, date = data.next()
-			ticks.append((price, volume))
-	except StopIteration:
-		return ticks
+    try:
+        while True:
+            price, volume, date = data.next()
+            ticks.append((price, volume))
+    except StopIteration:
+        return ticks
 
 
 def sliding_window(size, data_source):
-	i = 0
-	for i in range(0, len(data_source) - size):
-		window = []
-		for j in range(0, size):
-			window.append(data_source[i+j])
+    i = 0
+    for i in range(0, len(data_source) - size):
+        window = []
+        for j in range(0, size):
+            window.append(data_source[i + j])
 
-		yield window
+        yield window
+
 
 def normalize(v, _max, _min):
     return 2.0 / (_max - _min) * (v - _min) - 1.0
@@ -37,142 +38,139 @@ def normalize(v, _max, _min):
 def denormalize(v, _max, _min):
     return (v + 1.0) / 2.0 * (_max - _min) + _min
 
+
 def normalizer(window, normalize_func):
-	first_sample = window[0]
-	dimension = len(first_sample)
-	result = [[0.0]*dimension] * len(window)
+    first_sample = window[0]
+    dimension = len(first_sample)
+    result = [[0.0] * dimension] * len(window)
 
-	mins = [float('inf')] * dimension
-	maxs = [-float('inf')] * dimension
+    mins = [float('inf')] * dimension
+    maxs = [-float('inf')] * dimension
 
-	for sample in window:
-		for i in range(0, dimension):
-			mins[i] = min(mins[i], sample[i])
-			maxs[i] = max(maxs[i], sample[i])
+    for sample in window:
+        for i in range(0, dimension):
+            mins[i] = min(mins[i], sample[i])
+            maxs[i] = max(maxs[i], sample[i])
 
+    for i in range(0, len(window)):
+        for j in range(0, dimension):
+            result[i][j] = normalize_func(window[i][j], maxs[j], mins[j])
 
-	for i in range(0, len(window)):
-		for j in range(0, dimension):
-			result[i][j] = normalize_func(window[i][j], maxs[j], mins[j])
-
-	return result, mins, maxs
+    return result, mins, maxs
 
 
 def normalizeWindow(window):
-	return normalizer(window, normalize)
+    return normalizer(window, normalize)
+
 
 def denormalizeVector(vector, _max, _min):
-	result = [0.0] * len(vector)
-	for i in range(0, len(vector)):
-		result[i] = denormalize(vector[i], _max, _min)
-	return result
+    result = [0.0] * len(vector)
+    for i in range(0, len(vector)):
+        result[i] = denormalize(vector[i], _max, _min)
+    return result
+
 
 def create_network(data_dimension=2, history_interval=5, prediction_interval=5, hiddenLayersCount=3):
-	n = FeedForwardNetwork()
+    n = FeedForwardNetwork()
 
-	inLayer = SigmoidLayer(data_dimension * history_interval) # prices + volumes
-	hiddenLayers = []
+    inLayer = SigmoidLayer(data_dimension * history_interval)  # prices + volumes
+    hiddenLayers = []
 
-	for i in range(0, hiddenLayersCount):
-	    hiddenLayers.append(SigmoidLayer(data_dimension * history_interval))
+    for i in range(0, hiddenLayersCount):
+        hiddenLayers.append(SigmoidLayer(data_dimension * history_interval))
 
-	outLayer = SigmoidLayer(prediction_interval)
+    outLayer = SigmoidLayer(prediction_interval)
 
-	n.addInputModule(inLayer)
-	for layer in hiddenLayers:
-	    n.addModule(layer)
-	n.addOutputModule(outLayer)
+    n.addInputModule(inLayer)
+    for layer in hiddenLayers:
+        n.addModule(layer)
+    n.addOutputModule(outLayer)
 
-	n.addConnection(FullConnection(inLayer, hiddenLayers[0]))
+    n.addConnection(FullConnection(inLayer, hiddenLayers[0]))
 
-	for i in range(1, len(hiddenLayers)):
-	    n.addConnection(FullConnection(hiddenLayers[i - 1], hiddenLayers[i]))
+    for i in range(1, len(hiddenLayers)):
+        n.addConnection(FullConnection(hiddenLayers[i - 1], hiddenLayers[i]))
 
-	n.addConnection(FullConnection(hiddenLayers[len(hiddenLayers) - 1], outLayer))
-	n.sortModules()
-	return n
-
-def train_price_volume_sliding_window(network, training_set, data_dimension=2, price_index=0, history_interval=5, prediction_interval=5):
-	trainer = RPropMinusTrainer(network, verbose=True, batchlearning=True, learningrate=0.01, lrdecay=0.0, momentum=0.0, weightdecay=0.0)
-	dataset = SupervisedDataSet(history_interval * data_dimension, prediction_interval)
-	source_window = sliding_window(history_interval, training_set)
-	forecast_window = sliding_window(prediction_interval, training_set[history_interval-1:])
-
-	try:
-		while True:
-			ticks = source_window.next()
-			forecast_ticks = forecast_window.next()
-
-			samples = normalizeWindow(ticks)[0]
-			forecasts_ticks = normalizeWindow(forecast_ticks)[0]
-
-			forecasts = []
-
-			for forecast in forecasts_ticks:
-				forecasts.append(forecast[price_index])
+    n.addConnection(FullConnection(hiddenLayers[len(hiddenLayers) - 1], outLayer))
+    n.sortModules()
+    return n
 
 
-			
-			inputs = []
+def train_price_volume_sliding_window(network, training_set, data_dimension=2, price_index=0, history_interval=5,
+                                      prediction_interval=5):
+    trainer = RPropMinusTrainer(network, verbose=True, batchlearning=True, learningrate=0.01, lrdecay=0.0, momentum=0.0,
+                                weightdecay=0.0)
+    dataset = SupervisedDataSet(history_interval * data_dimension, prediction_interval)
+    source_window = sliding_window(history_interval, training_set)
+    forecast_window = sliding_window(prediction_interval, training_set[history_interval - 1:])
+
+    try:
+        while True:
+            ticks = source_window.next()
+            forecast_ticks = forecast_window.next()
+
+            samples = normalizeWindow(ticks)[0]
+            forecasts_ticks = normalizeWindow(forecast_ticks)[0]
+
+            forecasts = []
+
+            for forecast in forecasts_ticks:
+                forecasts.append(forecast[price_index])
+
+            inputs = []
+
+            flattenInput = [item for sublist in samples for item in sublist]
+            flattenOutput = forecasts
+
+            print 'Flatten input %s' % flattenInput
+            print 'Flatten ou %s' % flattenOutput
+
+            dataset.appendLinked(flattenInput, flattenOutput)
 
 
-			flattenInput = [item for sublist in samples for item in sublist]
-			flattenOutput = forecasts
-
-			print 'Flatten input %s' % flattenInput
-			print 'Flatten ou %s' % flattenOutput
-
-			dataset.appendLinked(flattenInput, flattenOutput)
-
-
-	except StopIteration:
-		trainer.setData(dataset)
-		trainer.trainUntilConvergence(validationProportion=0.50, maxEpochs=1000, verbose=False)
-
-
-
+    except StopIteration:
+        trainer.setData(dataset)
+        trainer.trainUntilConvergence(validationProportion=0.50, maxEpochs=1000, verbose=False)
 
 
 if __name__ == '__main__':
-	price_index = 0
-	history_interval = 30
+    price_index = 0
+    history_interval = 30
 
-	ticks = load_all_ticks()
+    ticks = load_all_ticks()
 
-	network = create_network(history_interval=history_interval)
+    network = create_network(history_interval=history_interval)
 
-	train_end = validate_start = 300
+    train_end = validate_start = 300
 
-	train_price_volume_sliding_window(network, ticks[0:train_end], history_interval=history_interval, price_index=0)
+    train_price_volume_sliding_window(network, ticks[0:train_end], history_interval=history_interval, price_index=0)
 
-	writer = csv.writer(open('nexttest.predictions.csv', 'w'), delimiter=',', quotechar='\"', quoting=csv.QUOTE_MINIMAL)
+    writer = csv.writer(open('nexttest.predictions.csv', 'w'), delimiter=',', quotechar='\"', quoting=csv.QUOTE_MINIMAL)
 
-	
-	days = validate_start
+    days = validate_start
 
-	for i in range(0, validate_start):
-		writer.writerow([i+1, 0.0])
+    for i in range(0, validate_start):
+        writer.writerow([i + 1, 0.0])
 
+    wnd = sliding_window(history_interval, ticks[validate_start:])
 
-	wnd = sliding_window(history_interval, ticks[validate_start:])
+    while True:
 
-	while True:
+        sample = wnd.next()
+        normalized, mins, maxs = normalizeWindow(sample)
 
-		sample = wnd.next()
-		normalized, mins, maxs = normalizeWindow(sample)
+        flattened = [item for sublist in normalized for item in sublist]
+        response = network.activate(flattened)
 
-		flattened = [item for sublist in normalized for item in sublist]
-		response = network.activate(flattened)
+        forecast_prices = denormalize(response, mins[0], maxs[0])
 
-		forecast_prices = denormalize(response, mins[0], maxs[0])
+        prices_output = []
+        for v in forecast_prices:
+            prices_output.append(float(v))
 
-		prices_output = []
-		for v in forecast_prices:
-			prices_output.append(float(v))
+        # for i in range(0,len(prices_output)):
+        writer.writerow([days + 1, prices_output[3]])
 
-		# for i in range(0,len(prices_output)):
-		writer.writerow([days+1, prices_output[3]])
-
-		days += 1
+        days += 1
 
 
